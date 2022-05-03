b2f63380262d2d744bfd0586ec9d0e583d767633
Mehul Bokdia <90055019+mehul-3001@users.noreply.github.com>
2022-05-04 00:51:15 +0530

    Update gym_db.sql

--- Database_Project/gym_db.sql
+++ Database_Project/gym_db.sql
@@ -1,21 +1,9 @@
--- phpMyAdmin SQL Dump
--- version 5.0.2
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Oct 21, 2020 at 09:52 AM
--- Server version: 10.4.14-MariaDB
--- PHP Version: 7.2.33
+
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `gym_db`
